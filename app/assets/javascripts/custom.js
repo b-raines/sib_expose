0bc467baf600565f6e67ec5bea6084535243fdb5
Brent Raines <bt.raines@gmail.com>
2013-12-13 02:25:37 -0500

    toggle caret

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,19 @@
+function rotate(element, degree) {
+    element.animate({
+                '-webkit-transform': 'rotate(' + degree + 'deg)',
+                '-moz-transform': 'rotate(' + degree + 'deg)',
+                '-ms-transform': 'rotate(' + degree + 'deg)',
+                '-o-transform': 'rotate(' + degree + 'deg)',
+                'transform': 'rotate(' + degree + 'deg)',
+                'zoom': 1
+    }, 5000);
+}
+
 $(document).ready(function() {
 	$(document.body).on('click', '.dropdown', function() {
 		event.preventDefault();
 		$(this).parent().find('.attendee-list').toggle('fast');
+		console.log($(this).find('span').html());
+		$(this).find('.caret').toggleClass('upside-down');
 	});
 });
