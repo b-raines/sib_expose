5caa468752bbf99dcc99f4a31aa7a46a692dfa93
Brent Raines <bt.raines@gmail.com>
2013-12-13 00:39:48 -0500

    add style

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.0.1'
 gem 'pg'
 gem 'eventbrite-client'
 gem 'zurb-foundation'
+gem 'clockwork'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,3 @@
-function rotate(element, degree) {
-	element.css({ WebkitTransform: 'rotate(' + degree + 'deg)' });
-	element.css({ '-moz-transform': 'rotate(' + degree + 'deg)' });
-}
-
 $(document).ready(function() {
 	$(document.body).on('click', '.dropdown', function() {
 		event.preventDefault();
